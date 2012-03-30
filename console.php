73c14cb933da1ec85c5a0e2a71f66a55dc27cfa8
Karthick L <alkarthick@stellarteam.in>
2012-03-30 18:44:58 +0530

    sfdsf

---
+++ console.php
@@ -0,0 +1,17 @@
+<?php
+
+// change the following paths if necessary
+$yii=dirname(__FILE__).'/library/yii/framework/yii.php';
+$config=dirname(__FILE__).'/protected/config/console.php';
+
+
+
+// remove the following lines when in production mode
+defined('YII_DEBUG') or define('YII_DEBUG',true);
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+define('ROOT_DIR',dirname(__FILE__));
+
+require_once($yii);
+Yii::createConsoleApplication($config)->run();
---
+++ js/initddsmoothmenu.js
@@ -0,0 +1,7 @@
+ddsmoothmenu.init({
+	mainmenuid: "smoothmenu1", 
+	orientation: 'h', 
+	classname: 'ddsmoothmenu', 
+	//customtheme: ["#1c5a80", "#18374a"],
+	contentsource: "markup" 
+})
